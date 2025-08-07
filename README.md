ae9b857d903c89e2c4feb4c57b4b685c4dbc6842
AlexEmanuelSoulis <ebalexsoulis@outlook.com>
2025-08-06 22:53:47 -0400

    final version

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Working-with-Data-Collections
+Project Breakdown
+##art 1 – Refactor CSV String into 2D Array
+The CSV string is split into lines, and then each line is split by commas to form a 2D array.
+
+## Part 2 – Count the Data Rows
+The program calculates how many data rows exist (excluding the header).
+
+## Part 3 – Convert 2D Array to Array of Objects
+The header row is converted to lowercase keys, and each data row becomes an object with key-value pairs.
+
+## Part 4 – Manipulate the Data
+The last object is removed
+
+A new object is added at the end
+
+A second object is replaced
+
+The average age is calculated across all entries
+
+## Part 5 – Convert Back to CSV
+The array of objects is transformed back into a properly formatted CSV string using map() and join().
+
--- script.js
+++ script.js
@@ -35,5 +35,42 @@ let arrObj = {
   data: objData
 };
 
+
+
+let lastElement = arrObj.data.pop();
+console.log(lastElement);
+
+arrObj.data[1] = {
+    id:"48",
+    name:"Barry",
+    occupation:"Runner",
+    age:25
+};
+
+arrObj.data.push( {
+    id:"7",
+    name:"Bilbo",
+    occupation:"none",
+    age:111
+})
+
+let avreageAge = 0;
+
+for(let inc_age = 0; inc_age<arrObj.data.length; inc_age++){
+    avreageAge += parseInt(arrObj.data[inc_age].age);
+}
+
+avreageAge = avreageAge / arrObj.data.length;
+
 console.log(arrObj.header);
 console.log(arrObj.data);
+console.log(avreageAge+ " is the average age");
+
+
+
+let finalCSV = [arrObj.header.join(",")].concat(
+    arrObj.data.map(obj => arrObj.header.map(key => obj[key]).join(","))
+  ).join("\n");
+  
+  console.log("CSV OUTPUT:\n" + finalCSV);
+  
